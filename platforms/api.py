1fa18461f671a35701904e04928bd9211855a64b
roddrick369 <rodolfohferreira@outlook.com>
2025-05-28 09:53:28 -0300

    feat: add learning_path retrieval api

--- platforms/api.py
+++ platforms/api.py
@@ -4,3 +4,6 @@ def get_courses() -> list: # type: ignore
     url = "https://portal.fullcycle.com.br/api/cursos/my.json"
     return get(url)  # type: ignore
 
+def get_learning_paths(course_id: int, classroom_id: int) -> dict:
+    url = f"https://portal.fullcycle.com.br/api/learning_paths/categories/{course_id}/classrooms/{classroom_id}"
+    return get(url)  # type: ignore
