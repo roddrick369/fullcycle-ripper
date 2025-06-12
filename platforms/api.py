8c781866d783881eea1380deeb56153a4f219218
roddrick369 <rodolfohferreira@outlook.com>
2025-06-11 23:07:19 -0300

    fix: update module list path and enhance data processing in get_modules function

--- platforms/api.py
+++ platforms/api.py
@@ -1,10 +1,11 @@
 import json
 from string import Template
 import os
+import re
 from utils.http_client import get
 from utils.json_extract import  decode
 
-module_list = os.path.join('result_module-list.json')
+module_list = os.path.join('result_module-list-2.json')
 
 endpoints_path = os.path.join('utils', 'endpoints.json')
 
@@ -43,10 +44,34 @@ def get_modules(chapter_id: int, classroom_id: int) -> dict:
     }
     parsed_data = Template(data["modules"]).substitute(values)
     url = parsed_data
-    encoded_res =  get(url) #type: ignore
-    res = decode(encoded_res["content"]) # type: ignore
-    decompressed_dict = json.loads(rf'''{{"content": {res}}}''')
-    with open(module_list, 'a', encoding='utf-8') as file:
-        json.dump(res, file, ensure_ascii=False, indent=2) # type: ignore
+    res =  decode(get(url)["content"]) #type: ignore
+    
+    #print(res)
+    # check_res = os.path.join('pure_res.json')
+    # with open(check_res, 'a', encoding='utf-8') as f:
+    #     # f.write(json.dumps(res, ensure_ascii=False, indent=4))
+    #     f.write(res)
+
+    html_re = re.compile(r'<.*?(?=\},\{|\},\\"|\}\]|,")')
+    #json_re = re.compile(r'"\[\{.*?\}\]"')
+    transcript_entry_re = re.compile(r'"(?:video_transcription_nivo.*?"(?=,"))')
+
+    res_html_removed = html_re.sub('\"', res)
+    clean_res = transcript_entry_re.sub('"video_transcription_nivo":""', res_html_removed) # type: ignore
+
+    #print(clean_res)
+    # check_res = os.path.join('check_res.json')
+    # with open(check_res, 'a', encoding='utf-8') as f:
+    #     f.write(json.dumps(clean_res, ensure_ascii=False, indent=4))
+
+    decompressed_dict = json.loads(clean_res)
+
+    print(decompressed_dict)
+
+    with open(module_list, 'a', encoding='utf-8') as f:
+        f.write(json.dumps(decompressed_dict, ensure_ascii=False, indent=4))
+    # decompressed_dict = json.loads(rf'''{{"content": {res}}}''')
+    # with open(module_list, 'a', encoding='utf-8') as file:
+    #     json.dump(res, file, ensure_ascii=False, indent=2) # type: ignore
         
-    return {"content": res}
+    return {"content": decompressed_dict}
