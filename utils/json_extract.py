acb79aa37562ffddc208b1a657968d2ba08f2fa6
roddrick369 <rodolfohferreira@outlook.com>
2025-06-11 23:06:08 -0300

    fix: correct string decoding in decompress function

--- utils/json_extract.py
+++ utils/json_extract.py
@@ -2,7 +2,7 @@ import base64
 import gzip
 
 def decompress(compressed_binary: bytes) -> str:
-    return gzip.decompress(compressed_binary).decode('unicode-escape') # type: ignore
+    return gzip.decompress(compressed_binary).decode('utf-8') # type: ignore
 
 def decode(encoded_string: str) -> str: # type: ignore
     decoded_str = base64.b64decode(encoded_string)
